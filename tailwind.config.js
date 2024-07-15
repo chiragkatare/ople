6254cb01cacbde4cc93be4a534d296d6f136cf3a
chiragkatare <katare.chirag@autoninja.in>
2024-07-15 11:41:32 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./src/**/*.{html,ts}",],
+  content: ["./src/**/*.{html,ts}",],
   theme: {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        fade: 'fadeOut 1s ease-in-out',
+      },
+
+
+      // that is actual animation
+      keyframes: theme => ({
+        fadeOut: {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+      }),
+      // fontSize{
+      //   '3xl':['calc((var(--heading-3-size-value) - 1) * 1.2vw + 1rem)',],
+      // },
+      fontSize: {
+        'h3': [
+          'calc(0.5 * 1.2vw + 1rem)', 'calc(1.2em * (1 + (1 - 1.5em)/25))'
+        ],
+        'h1': [
+          'calc(2.6 * 1.2vw + 1rem)', 'calc(1.2em * (1 + (1 - 1.5em)/25))'
+        ],
+
+        'h2': [
+          'calc(1 * 1.2vw + 1rem);'
+        ],
       },
       colors: {
         'blue-900': '#072B49',
@@ -16,4 +47,3 @@ module.exports = {
   },
   plugins: [],
 }
-
